3909bcc9fb6cb1553739130ca05b77133b082473
ibrahimtrahim <trahimbrahim@gmail.com>
2024-05-14 16:27:05 +0000

    home page & card component

--- countries/src/components/Header.js
+++ countries/src/components/Header.js
@@ -4,11 +4,11 @@ import Switcher from './Switcher';
 const Header = () => {
   return (
     <header>
-        <nav class="border-gray-200 shadow bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+        <nav class="border-gray-200 shadow bg-gray-50 dark:bg-gray-700 dark:border-gray-700">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Where in the world?</span>
+                <span className="self-center sm:text-sm md:text-2xl font-semibold whitespace-nowrap dark:text-white">Where in the world?</span>
                 <div className='flex items-center justify-between'>
-                  <div className='mr-2'><Switcher /></div><span class="text-l font-semibold whitespace-nowrap dark:text-white">Dark Mode</span>
+                  <div className='mr-2'><Switcher /></div><span class="sm:text-xs lg:text-base whitespace-nowrap dark:text-white">Dark Mode</span>
                 </div>
             </div>
         </nav>
--- countries/src/screens/Home.js
+++ countries/src/screens/Home.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
+import { Card } from '../components/Card';
 
 function Home() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-    <section className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-      <input type="text" id="search-navbar" className="block w-2/5 p-2 ps-10 text-sm text-gray-900 rounded-md shadow bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for a country..." />
+    <main className='dark:bg-slate-800'>
+    <section className='max-w-screen-xl flex flex-wrap items-center lg:justify-between mx-auto p-4'>
+      <input type="text" id="search-navbar" className="block mb-8 w-full md:w-2/5 p-2 ps-10 text-sm text-gray-900 rounded-md shadow bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for a country..." />
       <div>
         <button onClick={()=> setIsOpen((prev) => !prev)} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="relative text-gray-700 bg-gray-50 rounded-md shadow text-sm px-5 py-2.5 text-left inline-flex items-center dark:bg-gray-700 dark:text-white dark:focus:ring-blue-800" type="button">Filter by Region
         <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
@@ -29,10 +31,20 @@ function Home() {
           <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
         </li>
       </ul>
-  </div>
-      }
-      </div>
-    </section>
+    </div>
+        }
+        </div>
+      </section>
+      <section className='max-w-7xl mx-auto py-7'>
+        <div className='flex justify-between flex-wrap'>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </section>
+    </main>
     </>
   )
 }
